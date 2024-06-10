b8b3d799d847a81761748f4854db38c3c894634e
name <edgar.gzz10@gmail.com>
2024-06-10 17:07:42 -0600

    Configuration and Vuetify

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-app-bar title="My Movie Ratings"></v-app-bar>
+
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Rate movies"></v-list-item>
+        <v-list-item title="Add movie"></v-list-item>
+        <v-list-item title="Edit movie"></v-list-item>
+        <v-list-item title="Delete movie"></v-list-item>
+
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      <router-view></router-view>
+    </v-main>
+  </v-layout>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-footer height="40" app>
+    
+
+    <div
+      class="text-caption text-disabled"
+      style="position: absolute; right: 16px;"
+    >
+      &copy; {{ (new Date()).getFullYear() }} <span class="d-none d-sm-inline-block">Edgar</span>
+      —
+      <a
+        class="text-decoration-none on-surface"
+        href="https://www.linkedin.com/in/edgar-gonzalez10/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Edgar Gonzalez
+      </a>
+    </div>
+  </v-footer>
+</template>
+
+<script setup>
+  
+</script>
+
+<style scoped lang="sass">
+</style>
